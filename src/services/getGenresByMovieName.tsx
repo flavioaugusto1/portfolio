e0e09b2a63d494de43d41551a4c7ae24212944f2
Flávio <f.augustosdn@gmail.com>
2024-03-09 00:48:10 -0300

    feat: trazendo origem do filme

--- src/services/getGenresByMovieName.tsx
+++ src/services/getGenresByMovieName.tsx
@@ -4,6 +4,11 @@ import { movies } from "./movies";
 interface MovieGenresProps {
   data: {
     genres: GenreProps[];
+    production_countries: [
+      {
+        iso_3166_1: string;
+      },
+    ];
   };
 }
 
@@ -18,7 +23,13 @@ export async function getGenresByMovieName(movieId: number) {
       `/movie/${movieId}?language=pt-Br`,
     );
 
-    return data.genres;
+    const originMovie = data.production_countries.map((movie) => {
+      return movie.iso_3166_1;
+    });
+
+    const listDetailsMovie = [...data.genres, { origin: originMovie[0] }];
+
+    return listDetailsMovie;
   } catch (error) {
     toast.error("Não foi possível carregar os filmes");
     return [];
